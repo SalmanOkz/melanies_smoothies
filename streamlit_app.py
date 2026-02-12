79957a4e85af58b04b7072555c0bb23aeb65b183
Muhammad Salman <muhammad.59195@iqra.edu.pk>
2026-02-13 00:56:28 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if st.button("Submit Order"):
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiesfroot_response.json(), use_container_width=True)
